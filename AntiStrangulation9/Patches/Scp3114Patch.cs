ad297cec1f3780226eec410e661d6c91083643a2
AngelSeraphim <server@praniksl.ru>
2024-10-27 23:38:13 +0500

    9.0.0

---
+++ AntiStrangulation9/Patches/Scp3114Patch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using Mirror;
+using PlayerRoles.PlayableScps.Scp3114;
+using static PlayerRoles.PlayableScps.Scp3114.Scp3114Strangle;
+
+namespace AntiStrangulation.Patches
+{
+    [HarmonyPatch(typeof(Scp3114Strangle), nameof(Scp3114Strangle.ProcessAttackRequest))]
+
+    public class Scp3114Patch
+    {
+        static bool Prefix(Scp3114Strangle __instance, NetworkReader reader, ref StrangleTarget? __result)
+        {
+            __result = default;
+            return false;
+        }
+    }
+}
