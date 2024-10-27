e2da7820bfb24da71e1fbf1d113944cb37c4b474
AngelSeraphim <server@praniksl.ru>
2024-10-27 23:29:52 +0500

    Добавьте файлы проекта.

---
+++ AntiStrangulation/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using Exiled.API.Features;
+using HarmonyLib;
+
+namespace AntiStrangulation
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Prefix => "AntiStrangulation";
+        public override string Name => "AntiStrangulation";
+        public override string Author => "angelseraphim.";
+
+        public static Plugin plugin;
+        public static Harmony harmony;
+        public override void OnEnabled()
+        {
+            plugin = this;
+            harmony = new Harmony("AntiStrangulation");
+            harmony.PatchAll();
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            plugin = null;
+            harmony.UnpatchAll();
+            base.OnDisabled();
+        }
+    }
+}
